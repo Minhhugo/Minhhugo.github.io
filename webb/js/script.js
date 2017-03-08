77f691bf0356d22554d85e2b891ef95e9ec40b33
Minhhugo <minh21296@gmail.com>
2017-03-08 10:54:48 +0700

    C

--- webb/js/script.js
+++ webb/js/script.js
@@ -1,40 +1,5 @@
-$(function() {
 
-    $('#login-form-link').click(function(e) {
-        $("#login-form").delay(100).fadeIn(100);
-        $("#register-form").fadeOut(100);
-        $('#register-form-link').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
-    $('#register-form-link').click(function(e) {
-        $("#register-form").delay(100).fadeIn(100);
-        $("#login-form").fadeOut(100);
-        $('#login-form-link').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
 
-});
-$(function() {
-
-    $('#login-form-link1').click(function(e) {
-        $("#login-form1").delay(100).fadeIn(100);
-        $("#register-form1").fadeOut(100);
-        $('#register-form-link1').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
-    $('#register-form-link1').click(function(e) {
-        $("#register-form1").delay(100).fadeIn(100);
-        $("#login-form1").fadeOut(100);
-        $('#login-form-link1').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
-
-});
-//
 jQuery(document).ready(function($) {
     //selector đến menu cần làm việc
     var TopFixMenu = $("#menuNavbar");
