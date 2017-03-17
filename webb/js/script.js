242c444962be1e0d6b95f175a02532c449700cc0
Minhhugo <minh21296@gmail.com>
2017-03-17 11:51:12 +0700

    cc

--- webb/js/script.js
+++ webb/js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
         $(".slideanim").each(function(){
             var pos = $(this).offset().top;
             var winTop = $(window).scrollTop();
-            if(pos < winTop +400)
+            if(pos < winTop +600)
             {
                 $(this).addClass("slide1");
             }
